a1aa9fbf4ed736e50163d2f770148e1798780485
Włodzimierz Ciesielski <vovagorodok2@gmail.com>
2022-09-17 01:26:55 +0200

    More tools implementation

--- tools/uploader.py
+++ tools/uploader.py
@@ -5,51 +5,56 @@ OTA_SERVICE_UUID = "c68680a2-d922-11ec-bd40-7ff604147105"
 OTA_CHARACTERISTIC_UUID_RX = "c6868174-d922-11ec-bd41-c71bb0ce905a"
 OTA_CHARACTERISTIC_UUID_TX = "c6868246-d922-11ec-bd42-7b10244d223f"
 
-def scan_ota_devices(adapter_address=None, timeout=5.0):
-    for dongle in adapter.Adapter.available():
-        if adapter_address and adapter_address.upper() != dongle.address():
-            continue
+class Updater:
+    def __init__(self):
+        self.rx_char = None
+        self.tx_char = None
 
-        dongle.nearby_discovery(timeout=timeout)
+    def scan_ota_devices(self, adapter_address=None, timeout=5.0):
+        for dongle in adapter.Adapter.available():
+            if adapter_address and adapter_address.upper() != dongle.address():
+                continue
 
-        for dev in central.Central.available(dongle.address):
-            if OTA_SERVICE_UUID.lower() in dev.uuids:
-                yield dev
+            dongle.nearby_discovery(timeout=timeout)
 
-def connect_and_run(dev):
-    monitor = central.Central(adapter_addr=dev.adapter, device_addr=dev.address)
-    rx_char = monitor.add_characteristic(OTA_SERVICE_UUID, OTA_CHARACTERISTIC_UUID_RX)
-    tx_char = monitor.add_characteristic(OTA_SERVICE_UUID, OTA_CHARACTERISTIC_UUID_TX)
+            for dev in central.Central.available(dongle.address):
+                if OTA_SERVICE_UUID.lower() in dev.uuids:
+                    yield dev
 
-    print("Connecting to " + dev.alias)
-    monitor.connect()
-    if not monitor.connected:
-        print("Didn't connect to device!")
-        return
+    def connect_and_run(self, dev):
+        monitor = central.Central(adapter_addr=dev.adapter, device_addr=dev.address)
+        self.rx_char = monitor.add_characteristic(OTA_SERVICE_UUID, OTA_CHARACTERISTIC_UUID_RX)
+        self.tx_char = monitor.add_characteristic(OTA_SERVICE_UUID, OTA_CHARACTERISTIC_UUID_TX)
 
-    tx_char.start_notify()
-    tx_char.add_characteristic_cb(on_write)
-    rx_char.value = 0x00
+        print("Connecting to " + dev.alias)
+        monitor.connect()
+        if not monitor.connected:
+            print("Didn't connect to device!")
+            return
 
-    try:
-        monitor.run()
-    except KeyboardInterrupt:
-        print("Disconnecting")
+        self.tx_char.start_notify()
+        self.tx_char.add_characteristic_cb(self.on_write)
+        self.rx_char.value = 0x00
 
-    tx_char.stop_notify()
-    monitor.disconnect()
+        try:
+            monitor.run()
+        except KeyboardInterrupt:
+            print("Disconnecting")
 
+        self.tx_char.stop_notify()
+        monitor.disconnect()
 
-def on_write(iface, changed_props, invalidated_props):
-    value = changed_props.get('Value', None)
-    if not value:
-        return
+    def on_write(self, iface, changed_props, invalidated_props):
+        value = self.tx_char.value
+        if not value:
+            return
 
-    print("Recived: " + value)
+        print("Recived: " + value)
 
 if __name__ == '__main__':
-    devices = scan_ota_devices()
+    updater = Updater()
+    devices = updater.scan_ota_devices()
     for device in devices:
         print("OTA Device Found!")
-        connect_and_run(device)
+        updater.connect_and_run(device)
         break
