715c72772f810342ef91b65881e0657261af68c7
Włodzimierz Ciesielski <vovagorodok2@gmail.com>
2023-01-27 03:27:27 +0100

    Filter non esp example

--- examples/versioning/main.ino
+++ examples/versioning/main.ino
@@ -1,7 +1,11 @@
 #include <ArduinoBleOTA.h>
 
 #define DEVICE_NAME "ArduinoBleOTA"
+#if defined(ESP32)
 #define HW_NAME "Example Hardware"
+#else
+#define HW_NAME "Hardware"
+#endif
 #define HW_VER {1, 0, 0}
 #define SW_NAME "Example Software"
 #define SW_VER {1, 0, 0}
