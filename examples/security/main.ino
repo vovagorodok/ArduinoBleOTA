ee5e2013bdba09e8c6f4b93b728888ee93036b37
Włodzimierz Ciesielski <vovagorodok2@gmail.com>
2023-03-04 20:59:53 +0100

    Added security example

---
+++ examples/security/main.ino
@@ -0,0 +1,22 @@
+#include <ArduinoBleOTA.h>
+#include <BleOtaSecurityOnConnect.h>
+
+#ifdef USE_NIM_BLE_ARDUINO_LIB
+BleOtaSecurityOnConnect security;
+#endif
+
+void setup() {
+  ArduinoBleOTA.begin("ArduinoBleOTA", InternalStorage);
+
+#ifdef USE_NIM_BLE_ARDUINO_LIB
+  ArduinoBleOTA.setSecurity(security);
+  security.begin();
+#endif
+}
+
+void loop() {
+#ifndef USE_NIM_BLE_ARDUINO_LIB
+  BLE.poll();
+#endif
+  ArduinoBleOTA.pull();
+}
