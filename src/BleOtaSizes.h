517f4e2b06812979dfb6261d13aadb38aa15254c
Włodzimierz Ciesielski <vovagorodok2@gmail.com>
2023-08-03 00:18:37 +0200

    Attr size fix for ArduinoBle

--- src/BleOtaSizes.h
+++ src/BleOtaSizes.h
@@ -1,16 +1,20 @@
 #pragma once
 #include <ArduinoBleOTA.h>
 
+// BLE packet types:
+// https://microchipdeveloper.com/wireless:ble-link-layer-packet-types
+// MTU overhead:
+// https://docs.silabs.com/bluetooth/4.0/general/system-and-performance/throughput-with-bluetooth-low-energy-technology#attribute-protocol-att-operation
+#define BLE_OTA_MTU_WRITE_OVERHEAD 3
+
 #ifdef USE_NIM_BLE_ARDUINO_LIB
     #define BLE_OTA_MIN_MTU_SIZE BLE_ATT_MTU_DFLT
     #define BLE_OTA_MAX_MTU_SIZE BLE_ATT_MTU_MAX
     #define BLE_OTA_MAX_ATTR_SIZE BLE_ATT_ATTR_MAX_LEN
-    #define BLE_OTA_PACKAGES_NUM_IN_BUFFER 10
 #else
     #define BLE_OTA_MIN_MTU_SIZE 23
-    #define BLE_OTA_MAX_MTU_SIZE 143
-    #define BLE_OTA_MAX_ATTR_SIZE 128
-    #define BLE_OTA_PACKAGES_NUM_IN_BUFFER 10
+    #define BLE_OTA_MAX_MTU_SIZE 242
+    #define BLE_OTA_MAX_ATTR_SIZE (BLE_OTA_MAX_MTU_SIZE - BLE_OTA_MTU_WRITE_OVERHEAD)
 #endif
 
 #define BLE_OTA_HEADER_SIZE sizeof(uint8_t)
@@ -18,6 +22,7 @@
     #define BLE_OTA_ATTRIBUTE_SIZE BLE_OTA_MAX_ATTR_SIZE
 #endif
 #ifndef BLE_OTA_BUFFER_SIZE
+    #define BLE_OTA_PACKAGES_NUM_IN_BUFFER 10
     #define BLE_OTA_BUFFER_SIZE (BLE_OTA_ATTRIBUTE_SIZE - BLE_OTA_HEADER_SIZE) * BLE_OTA_PACKAGES_NUM_IN_BUFFER
 #endif
 
