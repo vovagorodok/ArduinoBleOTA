891b683b9b733cb536bddb0f5f71ef775cd0ee3e
Włodzimierz Ciesielski <vovagorodok2@gmail.com>
2024-03-15 13:41:08 +0100

    Updated storage defins

--- src/BleOtaStorage.h
+++ src/BleOtaStorage.h
@@ -5,10 +5,12 @@
 #if FLASHEND >= 0xFFFF
 #include "InternalStorageAVR.h"
 #endif
-#elif defined(STM32F1xx)
+#elif defined(ARDUINO_ARCH_STM32)
 #include <InternalStorageSTM32.h>
 #elif defined(ARDUINO_ARCH_RP2040)
 #include <InternalStorageRP2.h>
+#elif defined(ARDUINO_ARCH_RENESAS_UNO)
+#include <InternalStorageRenesas.h>
 #elif defined(ESP8266) || defined(ESP32)
 #include "InternalStorageESP.h"
 #else
