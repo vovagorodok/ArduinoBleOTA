8e1ba5e111d8fbc0e794b34e3b6e325955a45aa1
Włodzimierz Ciesielski <vovagorodok2@gmail.com>
2025-09-28 11:01:34 +0200

    Simplify BleOtaVersion

--- src/BleOtaVersion.h
+++ src/BleOtaVersion.h
@@ -1,23 +1,9 @@
 #pragma once
 #include <Arduino.h>
 
-#pragma pack(push, 1)
 struct BleOtaVersion
 {
-    constexpr BleOtaVersion():
-        major(),
-        minor(),
-        patch()
-    {}
-    constexpr BleOtaVersion(uint8_t major, uint8_t minor, uint8_t patch):
-        major(major),
-        minor(minor),
-        patch(patch)
-    {}
-
-private:
     uint8_t major;
     uint8_t minor;
     uint8_t patch;
 };
-#pragma pack(pop)
