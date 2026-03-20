9b400bbb2f374f4068f5922f26c2eb43ee7916ca
Florian <florian@florian-thinkpad.local>
2026-03-20 19:58:55 +0100

    Make multiservice example work with esp32 ble lib

--- examples/multiservice/multiservice.ino
+++ examples/multiservice/multiservice.ino
@@ -32,6 +32,16 @@ public:
 LedCharacteristicCallbacks ledCallbacks;
 #endif
 
+#ifdef BLE_OTA_BLE_LIB_ESP32
+class LedCharacteristicCallbacks: public BLECharacteristicCallbacks {
+public:
+  void onWrite(BLECharacteristic* characteristic) override {
+    digitalWrite(LED_BUILTIN, *characteristic->getData());
+  }
+};
+LedCharacteristicCallbacks ledCallbacks;
+#endif
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 
@@ -53,6 +63,21 @@ void setup() {
     NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::WRITE
   );
   ledCharacteristic->setValue(false);
+  ledCharacteristic->setCallbacks(&ledCallbacks);
+  ledService->start();
+  ArduinoBleOTA.begin(server, InternalStorage);
+  advertiseBle(server, NAME, BLE_OTA_SERVICE_UUID, LED_SERVICE_UUID);
+#endif
+
+#ifdef BLE_OTA_BLE_LIB_ESP32
+  auto server = initBle(NAME);
+  auto ledService = server->createService(LED_SERVICE_UUID);
+  auto ledCharacteristic = ledService->createCharacteristic(
+    LED_CHARACTERISTIC_UUID,
+    BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE
+  );
+  uint8_t value = 0;
+  ledCharacteristic->setValue(&value, sizeof(value));
   ledCharacteristic->setCallbacks(&ledCallbacks);
   ledService->start();
   ArduinoBleOTA.begin(server, InternalStorage);
